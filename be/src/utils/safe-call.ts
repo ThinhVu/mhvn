2d86a4f5cd2bc56e5eadf21d3e7a6e8c2cf74a92
ThinhVu <thinhvux@protonmail.com>
2025-06-07 06:11:08 +0700

    Add slow API warning with execution time logging
    Introduced logging for API calls exceeding 2000ms in execution time. Captures request metadata and logs it with a warning level to help identify performance bottlenecks. Errors during logging are safely caught and reported to avoid breaking functionality.

--- be/src/utils/safe-call.ts
+++ be/src/utils/safe-call.ts
@@ -1,11 +1,32 @@
 import {MiddlewareNext, Request, Response} from 'hyper-express';
+import dayjs from 'dayjs';
+import {getLogger} from "./logger";
 
 type SafeCallHandler<T> = (req: Request, res: Response, next: MiddlewareNext) => Promise<T>;
 type SafeCallResponse = (req: Request, res: Response, next: MiddlewareNext) => Promise<void>;
 
 export default function safeCall<T>(fn: SafeCallHandler<T>): SafeCallResponse {
   return async (req, res, next) => {
+    const then = dayjs()
     const rs = await fn(req, res, next)
+    const now = dayjs()
+    const diff = now.diff(then, 'millisecond')
+    if (diff > 2000) {
+      try {
+        const meta = {
+          locals: req.locals,
+          method: req.method,
+          path: req.path,
+          query: req.query_parameters,
+          params: req.path_parameters,
+          body: req.method === 'POST' || req.method === 'PUT' ? await req.json() : undefined,
+          diff
+        }
+        getLogger().warn('slow-api', meta)
+      } catch (e) {
+        console.error('safe-call', e)
+      }
+    }
     res.json(rs)
   }
 }
