f1fab8b74c249642bf7f23abfa86f7bfc0e44316
ThinhVu <thinhvux@protonmail.com>
2025-06-07 06:14:52 +0700

    Add utility functions for ObjectId and enhance common utils
    Introduced new utility functions for ObjectIds including `oidExcludes`, `mergeOidArrays`, and `interceptOidArrays`. Added `enumToObject`, `docs2Map`, and `urlFulfill` for improved common functionalities. Minor refactoring includes renaming `objectIdIncludes` to `oidIncludes` for consistency.

--- be/src/utils/common-util.ts
+++ be/src/utils/common-util.ts
@@ -1,6 +1,7 @@
 import {ObjectId} from "mongodb"
 // @ts-ignore
 import packageJson from "../../package.json"
+import _ from "lodash"
 
 export class ApiError extends Error {
   __API_ERROR__: boolean
@@ -31,14 +32,75 @@ export function getVersion() {
   return packageJson.version
 }
 
-export function objectIdIncludes(array: ObjectId[], item: ObjectId): boolean {
+export function oidIncludes(array: ObjectId[], item: ObjectId): boolean {
   if (!Array.isArray(array) || array.length === 0)
     return false
 
   for (const member of array) {
-    if (member.equals(item))
+    if (member && member.equals(item))
       return true
   }
 
   return false
+}
+
+export function oidExcludes(array: ObjectId[], excludeItems: ObjectId[]) {
+  if (_.isEmpty(array)) return []
+  if (_.isEmpty(excludeItems)) return array
+  const result = []
+  for (const member of array) {
+    if (oidIncludes(excludeItems, member))
+      continue
+    result.push(member)
+  }
+  return result
+}
+
+export function mergeOidArrays(array: ObjectId[], newItems: ObjectId[]) {
+  if (_.isEmpty(array)) return newItems
+  if (_.isEmpty(newItems)) return array
+  const result = []
+  for (const member of array) {
+    result.push(member)
+  }
+  for (const member of newItems) {
+    if (!oidIncludes(result, member))
+      result.push(member)
+  }
+  return result
+}
+
+export function interceptOidArrays(array: ObjectId[], newItems: ObjectId[]) {
+  if (_.isEmpty(newItems))
+    return []
+  const result = []
+  for (const member of array) {
+    if (oidIncludes(newItems, member))
+      result.push(member)
+  }
+  return result
+}
+
+export function enumToObject(enumObj: any): { [key: string]: string } {
+  return Object.keys(enumObj)
+    .filter(key => isNaN(Number(key))) // Filter out numeric keys if enum is numeric
+    .reduce((acc, key) => {
+      acc[key] = enumObj[key];
+      return acc;
+    }, {} as { [key: string]: string });
+}
+
+export function docs2Map<T extends Record<string, any>>(docs: Array<T>, getKey?: (item: T) => string): Record<string, T> {
+  if (!getKey)
+    getKey = (item: T) => item._id.toString()
+  return docs.reduce((map, item) => {
+    map[getKey(item)] = item;
+    return map;
+  }, {})
+}
+
+export function urlFulfill(url: string) {
+  if (!url) return
+  if (url.startsWith('http')) return url;
+  return `https://${url}`;
 }
