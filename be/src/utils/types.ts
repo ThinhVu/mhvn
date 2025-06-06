dd81710606c6ee33bc2b5d30801a7d5cfb14fbc3
ThinhVu <thinhvux@protonmail.com>
2025-06-07 06:16:43 +0700

    Add new utility types to `types.ts` for enhanced type support
    Introduced several new types including `TextIndex`, `RefId`, `Str`, and `I18nString` to extend the utility and flexibility of the type definitions. Imported `ObjectId` from MongoDB to support `RefId`. These additions aim to improve clarity and maintainability when defining and handling specific field types.

--- be/src/utils/types.ts
+++ be/src/utils/types.ts
@@ -1,6 +1,12 @@
+import {ObjectId} from 'mongodb';
+
 export type ComputedField<T> = T | null;
 export type VirtualField<T> = T | null;
 export type Indexed<T> = T;
 export type Unique<T> = T;
 export type HashedIndex<T> = T;
 export type SortedIndex<T> = T;
+export type TextIndex<T> = T;
+export type RefId<T> = ObjectId;
+export type Str<T> = string;
+export type I18nString = string;
