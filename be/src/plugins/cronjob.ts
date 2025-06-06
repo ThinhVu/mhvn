3d28285292fa85636d743a81c00a71e3bc6b61df
ThinhVu <thinhvux@protonmail.com>
2025-06-07 06:31:49 +0700

    Refactor cronjob scheduling and improve task handling.
    Introduced reusable cron patterns for better readability and consistency. Refactored task processing logic to use `Model.Tasks` directly, ensuring alignment with schema references. Improved code maintainability and reduced duplication.

--- be/src/plugins/cronjob.ts
+++ be/src/plugins/cronjob.ts
@@ -7,35 +7,52 @@ import {Model} from "../db/models";
 
 // https://crontab.guru
 export default async function cronjob() {
-   if (!process.env.RUN_CRONJOB) return
-   getLogger().info('[plugin] cronjob')
-   const Tasks = Model.Tasks;
+  if (!process.env.RUN_CRONJOB) return
 
-   new CronJob('*/12 * * * *' /* runEach5Minutes */, async () => {
-      const tasks = await Tasks.find({completed: false, failed: false, running: false, at: {$lte: new Date()}}).toArray()
-      for (const task of tasks) {
-         const {_id, type, metadata} = task
-         try {
-            Tasks.updateOne({_id}, {$set: {running: true}})
-            .then(async () => await appHook.trigger(`task:${type}`, metadata))
-            .then(async () => await Tasks.updateOne({_id}, {$set: {completed: true, running: false}}))
-            .catch(e => getLogger().error(e.message, {fn: 'cronjob::Tasks.updateOne:try'}))
-         } catch (e) {
-            Tasks.updateOne({_id}, {$set: {failed: true, error: e.message, running: false}}).catch(
-               e => getLogger().error(e.message, {fn: 'cronjob::Tasks.updateOne:catch'}))
-         }
+  const pattern = {
+    runAt8AM: '0 8 * * *',
+    runAt12H: '0 12 * * *',
+    runAt23H: '0 23 * * *',
+    runAtNewDay: '1 0 * * *',
+    runEveryMinutes: '* * * * *',
+    runEach5Minutes: '*/5 * * * *',
+    runEach10Minutes: '*/10 * * * *',
+    runEveryHour: '0 * * * *',
+    atEach4Hours: '0 */4 * * *',
+    runDailyAtNight: '58 11 * * *'
+  }
+
+  getLogger().info('[plugin] cronjob')
+
+  new CronJob(pattern.runEach5Minutes, async () => {
+    const tasks = await Model.Tasks.find({
+      completed: false,
+      failed: false,
+      running: false,
+      at: {$lte: new Date()}
+    }).toArray()
+    for (const task of tasks) {
+      const {_id, type, metadata} = task
+      try {
+        Model.Tasks.updateOne({_id}, {$set: {running: true}})
+          .then(async () => await appHook.trigger(`task:${type}`, metadata))
+          .then(async () => await Model.Tasks.updateOne({_id}, {$set: {completed: true, running: false}}))
+          .catch(e => getLogger().error(e.message, {fn: 'cronjob::Tasks.updateOne:try'}))
+      } catch (e) {
+        Model.Tasks.updateOne({_id}, {$set: {failed: true, error: e.message, running: false}}).catch(
+          e => getLogger().error(e.message, {fn: 'cronjob::Tasks.updateOne:catch'}))
       }
-   }, null, true)
+    }
+  }, null, true)
 
-   // new CronJob('0 * * * *' /* runEachHour */, () => {}, null, true)
-   // new CronJob('0 0 * * *' /* run at new day */, () => {}, null, true)
+  // new CronJob(pattern.runEveryHour, () => {}, null, true)
+  // new CronJob(pattern.runAtNewDay, () => {}, null, true)
 
-   const runDailyAtNight = '50 11 * * *'
-   new CronJob(runDailyAtNight, () => {
-      appMetricSnapshot()
-      userMetricSnapshot()
-   }, null, true)
+  new CronJob(pattern.runDailyAtNight, () => {
+    appMetricSnapshot()
+    userMetricSnapshot()
+  }, null, true)
 
-   appMetricSnapshot()
-   userMetricSnapshot()
+  appMetricSnapshot()
+  userMetricSnapshot()
 }
